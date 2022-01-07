0e9fd948f76fb5d84ee5ca3552f20b831c13df42
crystalct <nippurct@gmail.com>
2022-01-07 15:51:08 +0100

    Update RetroArchPatch.sh

--- RetroArchPatch.sh
+++ RetroArchPatch.sh
@@ -3,7 +3,7 @@
 sed ':a;N;$!ba;s/#elif defined(WIIU)/#elif defined(__PS3__)\n#define DEFAULT_BUILDBOT_SERVER_URL "http:\/\/cristaldi.github.unict.it\/"\n#elif defined(WIIU)/' RetroArch_Orig/config.def.h > config.def.h.mod
 cat config.def.h.mod > RetroArch_Orig/config.def.h
 rm config.def.h.mod
-sed ':a;N;$!ba;s/config_set_defaults(\&p_rarch->g_extern);/config_set_defaults(\&p_rarch->g_extern);\n         command_event(CMD_EVENT_RESTART_RETROARCH, NULL);/' RetroArch_Orig/retroarch.c > retroarch.c.mod
+sed ':a;N;$!ba;s/config_set_defaults(global_get_ptr());/config_set_defaults(global_get_ptr());\n         command_event(CMD_EVENT_RESTART_RETROARCH, NULL);/' RetroArch_Orig/retroarch.c > retroarch.c.mod
 sed ':a;N;$!ba;s/case CMD_EVENT_UNLOAD_CORE:/case CMD_EVENT_UNLOAD_CORE:\n         command_event(CMD_EVENT_RESTART_RETROARCH, NULL);\n         break;/' retroarch.c.mod > RetroArch_Orig/retroarch.c
 #cat retroarch.c.mod > RetroArch_Orig/retroarch.c
 rm retroarch.c.mod
