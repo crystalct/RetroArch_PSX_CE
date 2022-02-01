41fdf7f0d82b428aa90fe7b79afe2d69f6da580b
CrystalCT <nippurct@gmail.com>
2022-02-01 12:01:06 +0100

    Update for RA 1.10.x

--- RetroArchPatch.sh
+++ RetroArchPatch.sh
@@ -4,9 +4,9 @@ count=`sed -n ':a;N;$!ba;s/.*\(#elif defined(WIIU)\).*/\1/p' RetroArch_Orig/conf
 sed ':a;N;$!ba;s/#elif defined(WIIU)/#elif defined(__PS3__)\n#define DEFAULT_BUILDBOT_SERVER_URL "http:\/\/cristaldi.github.unict.it\/"\n#elif defined(WIIU)/' RetroArch_Orig/config.def.h > config.def.h.mod
 cat config.def.h.mod > RetroArch_Orig/config.def.h
 rm config.def.h.mod
-count=`grep "config_set_defaults(global_get_ptr())" retroarch.c | wc -l`; if [[ $count -eq 0 ]]; then echo "Errore global_get_ptr()"; fi
+count=`grep "config_set_defaults(global_get_ptr())" RetroArch_Orig/retroarch.c | wc -l`; if [[ $count -eq 0 ]]; then echo "Errore global_get_ptr()"; fi
 sed ':a;N;$!ba;s/config_set_defaults(global_get_ptr());/config_set_defaults(global_get_ptr());\n         command_event(CMD_EVENT_RESTART_RETROARCH, NULL);/' RetroArch_Orig/retroarch.c > retroarch.c.mod
-count=`grep "case CMD_EVENT_UNLOAD_CORE:" retroarch.c | wc -l`; if [[ $count -eq 0 ]]; then echo "CMD_EVENT_UNLOAD_CORE"; fi
+count=`grep "case CMD_EVENT_UNLOAD_CORE:" RetroArch_Orig/retroarch.c | wc -l`; if [[ $count -eq 0 ]]; then echo "Errore CMD_EVENT_UNLOAD_CORE"; fi
 sed ':a;N;$!ba;s/case CMD_EVENT_UNLOAD_CORE:/case CMD_EVENT_UNLOAD_CORE:\n         command_event(CMD_EVENT_RESTART_RETROARCH, NULL);\n         break;/' retroarch.c.mod > RetroArch_Orig/retroarch.c
 #cat retroarch.c.mod > RetroArch_Orig/retroarch.c
 rm retroarch.c.mod
